f3a655b099ce902a4d7824bc0f69f2a436176229
Hunter Cash <hmclaug@gmail.com>
2023-10-18 20:46:43 -0400

    hsow issue details

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Button, Table } from "@radix-ui/themes";
+import { Table } from "@radix-ui/themes";
 import Link from "next/link";
 import prisma from "@/prisma/client";
-import IssueStatusBadge from "../components/issueStatusBadge";
+import IssueStatusBadge from "../components/IssueStatusBadge";
 import delay from "delay";
 import IssueActions from "./IssueActions";
 
@@ -29,7 +29,7 @@ const IssuesPage = async () => {
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                {issue.title}
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
                 <div className="block md:hidden">
                   <IssueStatusBadge status={issue.status} />
                 </div>
